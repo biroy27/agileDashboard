d050fac9dcb8f499b48b62bad798314947051ae7
Bishu Roy <bishuthebest@gmail.com>
2019-03-01 17:16:14 +0000

    Dev_1.0

---
+++ agileDashBoard/src/main/java/com/tcs/agileDashBoard/AgileDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.agileDashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileDashBoardApplication.class, args);
+	}
+
+}
